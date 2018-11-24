d91593273c41922a8b415d6ff57698ce3cc39216
Bandyta <lukasz.grymulski@gmail.com>
2018-11-24 14:07:03 +0100

    lekcja enum ciag dalszy

---
+++ src/L3TypyWyliczenioweEnum3/Demo.java
@@ -0,0 +1,39 @@
+package L3TypyWyliczenioweEnum3;
+
+import L1TypyWyliczenioweEnum.Planety;
+
+//typy wyliczeniowe dziedzicz¹ z klasy Enum i mo¿emy wykorzystaæ kilka przydatnych metod z tej klasy
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Planety3 p1, p2, p3;
+		
+		//metoda ordinal() klasy Enum zwraca pozycje obiektu w wyliczeniu
+		System.out.println("==== Metoda ordinal() =====");
+		for (Planety p : Planety.values()) {
+			System.out.println(p + ": " + p.ordinal());
+		}
+		
+		//metoda compareTo() 
+		System.out.println("\n==== Metoda compareTo() =====");
+		p1=Planety3.Neptun;
+		p2=Planety3.Wenus;
+		p3=Planety3.Neptun;
+		System.out.println("Wywo³anie metody p2.compareTo(p1): " + p2.compareTo(p1));
+				
+		if (p1.compareTo(p3) == 0) {
+			System.out.println(p1 + " znajduje sie w takiej samej odleg³oœci od s³oñca, jak " + p2);
+		}
+		
+		//metoda equals() 
+		System.out.println("\n==== Metoda equals() =====");
+		if (p1.equals(p3)) {
+			System.out.println("OK, it is equal");
+		} else {
+			System.out.println("not equal");
+		}
+
+	}
+
+}
---
+++ src/L3TypyWyliczenioweEnum3/Planety3.java
@@ -0,0 +1,5 @@
+package L3TypyWyliczenioweEnum3;
+
+public enum Planety3 {
+	Merkury, Wenus, Ziemia, Mars, Jowisz, Saturn, Uran, Neptun
+}
