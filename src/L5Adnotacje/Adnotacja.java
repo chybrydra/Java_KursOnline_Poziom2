2cb992e021c67b2248b55f138b52754636d069da
Bandyta <lukasz.grymulski@gmail.com>
2018-11-25 23:57:56 +0100

    adnotacje poczatek

---
+++ src/L5Adnotacje/Adnotacja.java
@@ -0,0 +1,37 @@
+package L5Adnotacje;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME) //strategia zachowania adnotacji
+public @interface Adnotacja {
+	String login();
+	String password();	
+}
+
+/*
+adnotacje nie mog¹ u¿ywaæ klauzuli extends
+wzystkie adnotacje rozszerzaj¹ interfejs Annotation (robi¹ to automatycznie)
+adnotacje sk³adaj¹ siê tylko z deklaracji metod, ale bez cia³ metod!
+metody zachowuj¹ siê za to jak zmienne
+ 
+Okreœlenie zachowania adnotacji:
+Typ Wyliczeniowy: java.lang.annotation.RetentionPolicy
+SOURCE - zachowanie tylko w pliku .java
+CLASS - zachowanie równie¿ w pliku .class
+RUNTIME - dostêpnoœæ dla JVM
+
+Okreœlenie gdzie mo¿e byæ u¿yta metoda:
+Typ wyliczeniowy: java.lang.annotation.ElementType
+ANNOTATION_TYPE - inna adnotacja
+CONSTRUCTOR - konstruktor
+FIELD - pole
+LOCAL_VARIABLE - zmienna lokalna
+METHOD - metoda
+PACKAGE - pakiet
+PARAMETER - parametr
+TYPE - klasa/interfejs/wyliczenie
+*/
---
+++ src/L5Adnotacje/Sterowniki.java
@@ -0,0 +1,8 @@
+package L5Adnotacje;
+
+public class Sterowniki {
+	@Adnotacja(login = "rootSterownikA", password = "passwordSterownikA")
+	public void sterownikA() {
+		
+	}
+}
