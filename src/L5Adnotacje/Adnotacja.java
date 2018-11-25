be15452ff2eea8f652620eaadca671e93189c00e
Bandyta <lukasz.grymulski@gmail.com>
2018-11-26 00:08:26 +0100

    pobranie adnotacji dla metody

--- src/L5Adnotacje/Adnotacja.java
+++ src/L5Adnotacje/Adnotacja.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD,ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME) //strategia zachowania adnotacji
 public @interface Adnotacja {
 	String login();
--- src/L5Adnotacje/Sterowniki.java
+++ src/L5Adnotacje/Sterowniki.java
@@ -1,8 +1,36 @@
 package L5Adnotacje;
 
+import java.lang.reflect.Method;
+
 public class Sterowniki {
+	
+	private String login;
+	private String password;
+	
 	@Adnotacja(login = "rootSterownikA", password = "passwordSterownikA")
-	public void sterownikA() {
-		
+	public void sterownikA(String login, String password) {
+		this.login = login;
+		this.password = password;
+	}
+	
+	public static void main(String[] args) {
+		try {
+			//w mainie bêdziemy chcieli pobraæ adnotacje dla metody sterownikA
+			
+			//pobieranie obiektu reprezentuj¹cego nasz¹ klasê sterowniki i przypisanie go do klasy Class
+			Class<Sterowniki> c = Sterowniki.class; //pobieramy obiekt reprezentuj¹cy nasz¹ klasê do zmiennej c
+			
+			//pobranie informacji na temat methody klasy sterownikA, musimy przekazaæ jako obiekt metody class
+			Method m = c.getMethod("sterownikA", String.class, String.class);
+			
+			//pobranie informacji o naszej adnotacji:
+			Adnotacja adn = m.getAnnotation(Adnotacja.class);
+			
+			//wyœwietlenie informacji:
+			System.out.println(adn.login() + " " + adn.password());
+			
+		} catch (NoSuchMethodException e) {
+			System.out.println("Nie znaleziono metody.");
+		}
 	}
 }
