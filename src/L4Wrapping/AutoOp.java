6b00ef5b5128e072b9d29d4c444577403df49301
Bandyta <lukasz.grymulski@gmail.com>
2018-11-25 23:27:21 +0100

    autowrapping c.d.

--- src/L4Wrapping/AutoOp.java
+++ src/L4Wrapping/AutoOp.java
@@ -1,10 +1,14 @@
 package L4Wrapping;
 
+//AUTOWRAPPING
 //w javie od wersji 5 zmienne s¹ opakowywane (wrapped) automatycznie i automatycznie "rozpakowywane"
 //poni¿ej przyk³ady praktyczne
 
 public class AutoOp {
 	public static void main(String[] args) {
+		
+		// czêœæ pierwsza
+		System.out.println("CZÊŒÆ 1");	
 		Integer liczba = 100; //bez new!
 		Character znaczek = '8'; //bez new!
 		Boolean log = Boolean.TRUE; //bez new!
@@ -14,5 +18,35 @@ public class AutoOp {
 		boolean l = log; //nie trzeba wywo³ywaæ metody klasy Boolean!
 		
 		System.out.println("Integer: " + i + "\nCharacter: " + z + "\nBoolean: " + l);
+		
+		
+		// czêœæ druga
+		System.out.println("\nCZÊŒÆ 2");		
+		Integer liczba2 = sendInt(100);  //metoda przyjmuje Integer, da³em jej int, a mimo to automatycznie wrappuje
+		Character znaczek2 = sendChar('x'); //metoda przyjmuje Character, da³em jej char, a mimo to automatycznie ogarnia temat
+		System.out.println("liczba2: " + liczba2 + "\nznaczek2: " + znaczek2); //wszystko dzi¹³a jak nale¿y
+		
+		// czêœæ trzecia
+		System.out.println("\nCZÊŒÆ 3"); //pokazuje, ¿e dzia³ania wykonywane na tych klasach s¹ wykonywane z autowrappingiem, 
+		//java sama sobie rozpakowuje do typu prostego a potem znowu zapakowuje
+		Integer o1,o2;
+		int x;
+		o1 = 36;
+		o2 = o1/3;
+		System.out.println("Wynik wyra¿enia o1/3: " + o2);
+		++o2;
+		System.out.println("Wynik wyra¿enia ++o2: " + o2);
+		x = (o1/(o2-1))*23;
+		System.out.println("(o1/(o2-1))*23 = " + x);
+		
 	}
+	
+	static int sendInt(Integer value) {
+		return value;
+	}
+	
+	static char sendChar(Character value) {
+		return value;
+	}
+	
 }
