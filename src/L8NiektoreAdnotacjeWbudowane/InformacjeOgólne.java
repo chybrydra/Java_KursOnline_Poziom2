a3e25e1b73ec51dace0aca6b8a55799dd95ef5d3
Bandyta <lukasz.grymulski@gmail.com>
2018-11-27 00:05:02 +0100

    podstawowe adnotacje wbudowane w jave

---
+++ src/L8NiektoreAdnotacjeWbudowane/InformacjeOgólne.java
@@ -0,0 +1,15 @@
+package L8NiektoreAdnotacjeWbudowane;
+
+public class InformacjeOgólne {
+	/*
+	Przyk³adowe wbudowane w javie adnotacje (najbardziej popularne):
+	@Override			- informuje o koniecznoœci przykrycia dla oznaczonej metody
+	@Retention			- okreœla strategiê zachowania dla innych adnotacji
+	@Documented			- informuja narzêdzia o koniecznoœci udokumentowania adnotacji
+	@Target				- okreœla typy deklaracji dla adnotajci
+	@Inherited			- powoduje dziedziczenie adnotacji klasy bazowej przez jej podklasê
+	@Deprecated			- wskazuje przestarza³¹ deklaracjê 
+	@SafeVarargs		- oznacza, ¿e deklaracja NIE zawiera ¿adnych niebezpiecznych dzia³añ zwi¹zanych z 'vararg'
+	@SuppressWarnings	- wymusza wstrzymanie generowania okreœlonych ostrze¿eñ kompilatora
+	*/
+}
