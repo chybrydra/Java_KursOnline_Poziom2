52d809817e1702ede5ebe9ad98ee109065ebcbfa
Bandyta <lukasz.grymulski@gmail.com>
2018-11-26 23:57:58 +0100

    fixed tiny syntax error

--- src/L7AdnotacjeJednoelementowe/Adnotacja.java
+++ src/L7AdnotacjeJednoelementowe/Adnotacja.java
@@ -1,5 +1,12 @@
 package L7AdnotacjeJednoelementowe;
 
-public @interface Adnotacja {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface Adnotacja {
+	String value();
 }
