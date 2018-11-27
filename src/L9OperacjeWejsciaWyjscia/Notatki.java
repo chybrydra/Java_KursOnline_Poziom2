1dc877a9347c7046c7c14f6ac7b600c0061675f0
Bandyta <lukasz.grymulski@gmail.com>
2018-11-27 13:51:42 +0100

    input output stream notatki

---
+++ src/L9OperacjeWejsciaWyjscia/Notatki.java
@@ -0,0 +1,15 @@
+package L9OperacjeWejsciaWyjscia;
+
+public class Notatki {
+	// podstawowy system wejœcia/wyjœcia jest w pakiecie java.io.*
+	// operacje wyjœcia/wejœcia wykonuj¹ siê za pomoc¹ strumieni
+	// strumieñ to "abstrakcyjny byt który produkuje i konsumuje dane"
+	// strumieñ wejœcia s³u¿y do odczytu danych z pliku/klawiatury/gniazdka sieciowego itd.
+	// strumieñ wyjœcia wys³a dane do pliku/na nasza konsolê/do innego komputera
+	// operacje wejœcia/wyjœcia mog¹ odbywaæ siê równie¿ za pomoc¹ buforów i kana³ów,
+	// w tej lekcji bêd¹ jednak jedynie strumienie
+	// dwa podstawowe typy strumieni to strumienie znakowe i strumienie bajtowe
+	// strumienie znakowe s¹ jedynie u³atwieniem w przekazywaniu znaków,
+	// bo tak naprawdê wszystko siê sprowadza na najni¿szym poziomie do pracy na bajtach
+	//zarówno strumieñ bajtowy jak i znakowy maj¹ swoje klasy, których jest bardzo du¿o
+}
